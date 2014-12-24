5f82841b78529d352055ad9d7b7b165a28a2e69f
DeOldSax <DeOldSax@gmx.de>
2014-12-24 16:36:11 +0100

    redesigned overlay icon

--- js/info-overlay.js
+++ js/info-overlay.js
@@ -1,142 +1,159 @@
 $(function() {
-	
+	var isClickDisabled = false;
+
+	// hide all info-overlays 
+	var hideClasses = [".an-fade", ".grow", "info-overlay.manual span"];
 	$(".info-overlay-box .info-overlay").each(function() {
-		if ($(this).is(".an-fade") || $(this).is(".grow")) {
-			$(this).hide();
+		for (var i = 0; i < hideClasses.length; i++) {
+			if ($(this).is(hideClasses[i])) {
+				$(this).hide();
+			}
 		}
 	});
 
+	// shrink all manual overlays
+	$(".info-overlay-box .info-overlay-top.manual").height("20px");
+	$(".info-overlay-box .info-overlay-bottom.manual").height("20px");
+	$(".info-overlay-box .info-overlay-left.manual").width("20px");
+	$(".info-overlay-box .info-overlay-right.manual").width("20px");
 
+	// hide text on manual info-overlays
+	$(".info-overlay.manual").css("color", "transparent");
 
+	// show icons
+	$(".info-overlay.manual span").show();
+
+	// add cursor:pointer class
+	$(".info-overlay.manual").addClass("pointer");
+
+	// hover over info-overlay
 	$(".info-overlay-box").hover(function() {
+		if($(this).find(".info-overlay").is(".manual.hover")) {
+			toggle($(this).find(".info-overlay"));
+			isClickDisabled = true;
+			return;
+		}
+
 		var el = $(this).find(".info-overlay");
-		var param;
+
 		if (el.is(".an-fade")) {
 			el.fadeIn();
 		} else if (el.is(".grow")) {
-			if (el.is(".info-overlay-top")) {
-				param = {opacity:1, height: "toggle"};
-			} else if (el.is(".info-overlay-bottom")) {
-				param = {opacity:1, height: "toggle"};
-			} else if (el.is(".info-overlay-left")) {
-				param = {opacity:1, width: "toggle"};
-			} else if (el.is(".info-overlay-right")) {
-				param = {opacity:1, width: "toggle"};
-			}
-
-			el.animate(param);
+			growUp(el);
 		}
 			
 	}, function() {
+		if($(this).find(".info-overlay").is(".manual.hover")) {
+			toggle($(this).find(".info-overlay"));
+			isClickDisabled = false;
+			return;
+		}
 		var el = $(this).find(".info-overlay");
+
 		if (el.is(".an-fade")) {
-			$(this).find(".info-overlay").fadeOut();
+			el.fadeOut();
 		} else if (el.is(".grow")) {
-			if (el.is(".info-overlay-top")) {
-				param = {opacity:0, height: "toggle"};
-			} else if (el.is(".info-overlay-bottom")) {
-				param = {opacity:0, height: "toggle"};
-			} else if (el.is(".info-overlay-left")) {
-				param = {opacity:0, width: "toggle"};
-			} else if (el.is(".info-overlay-right")) {
-				param = {opacity:0, width: "toggle"};
-			}
-
-			el.animate(param);
-
+			growDown(el);
 		}
 	});
 	
-	// add icons
-	$(".info-overlay-box .info-overlay-top .toggle").html("<span class=\"glyphicon glyphicon-chevron-down\"></span>");
-	$(".info-overlay-box .info-overlay-bottom .toggle").html("<span class=\"glyphicon glyphicon-chevron-up\"></span>");
-	$(".info-overlay-box .info-overlay-right .toggle").html("<span class=\"glyphicon glyphicon-chevron-left\"></span>");
-	$(".info-overlay-box .info-overlay-left .toggle").html("<span class=\"glyphicon glyphicon-chevron-right\"></span>");
-
-	// hide overlay
-	$(".info-overlay-box .info-overlay-top:has(.toggle)").height($(this).find(".toggle").height());
-	$(".info-overlay-box .info-overlay-bottom:has(.toggle)").height($(this).find(".toggle").height());
-	$(".info-overlay-box .info-overlay-left:has(.toggle)").width("20px");
-	$(".info-overlay-box .info-overlay-right:has(.toggle)").width("20px");
-
-	// hide text
-	$(".info-overlay-box .info-overlay:has(.toggle)").css("color", "transparent");
-
-	// add cursor:pointer class
-	$(".info-overlay-box .info-overlay:has(.toggle)").hover(function() {
-		$(this).addClass("toggleHover");
-	}, function() {
-		$(this).removeClass("toggleHover");
+	// click on manual info-overlay
+	$(".info-overlay-box .info-overlay.manual").click(function() {
+		if (!isClickDisabled) {
+			toggle($(this));
+		}
 	});
-
-	$(".info-overlay-box .info-overlay:has(.toggle)").click(function() {
-		toggle($(this));
-	});
-
+	
 });
 
-function toggle(popover) {
-		var hardcodedWidth = 20;
-		var toggle = popover.find(".toggle");
-		var param;
-		var symbol;
-		var color;
-						
-		if (popover.height() == toggle.height() || popover.width() == hardcodedWidth) {
-			/* OPEN Popover */
-			console.log("popver width = toggle width");
-			// add icons
-			if(popover.is(".info-overlay-bottom")) {
-				var height = popover.css("height", "auto").height();
-				popover.height(toggle.height());
-				symbol = "glyphicon glyphicon-chevron-down";
-				param = {height: height};
-			} else if(popover.is(".info-overlay-top")) {
-				var height = popover.css("height", "auto").height();
-				popover.height(toggle.height());
-				symbol = "glyphicon glyphicon-chevron-up";
-				param = {height: height};
-			} else if(popover.is(".info-overlay-right")) {
-				var width = popover.css("width", "auto").width();
-				popover.width(hardcodedWidth + "px");
-				symbol = "glyphicon glyphicon-chevron-right";
-				param = {width: width};
-			} else if(popover.is(".info-overlay-left")) {
-				var width = popover.css("width", "auto").width();
-				console.log("width: " + width);
-				popover.width(hardcodedWidth + "px");
-				symbol = "glyphicon glyphicon-chevron-left";
-				param = {width: width};
-			}
+function growUp(el) {
+	var param;
 
-			color = "black";
-			popover.css("color", color);
-			popover.animate(param)
+	if (el.is(".info-overlay-top")) {
+		param = {opacity:1, height: "toggle"};
+	} else if (el.is(".info-overlay-bottom")) {
+		param = {opacity:1, height: "toggle"};
+	} else if (el.is(".info-overlay-left")) {
+		param = {opacity:1, width: "toggle"};
+	} else if (el.is(".info-overlay-right")) {
+		param = {opacity:1, width: "toggle"};
+	}
 
-		} else {
-			/* Close Popover */
-			console.log("popver width != toggle width");
-			if(popover.is(".info-overlay-bottom")) {
-				symbol = "glyphicon glyphicon-chevron-up";
-				param = {height: toggle.height()};
-			} else if(popover.is(".info-overlay-top")) {
-				symbol = "glyphicon glyphicon-chevron-down";
-				param = {height: toggle.height()};
-			} else if(popover.is(".info-overlay-right")) {
-				symbol = "glyphicon glyphicon-chevron-left";
-				param = {width: hardcodedWidth + "px"};
-			} else if(popover.is(".info-overlay-left")) {
-				symbol = "glyphicon glyphicon-chevron-right";
-				param = {width: hardcodedWidth + "px"};
-			}
+	el.animate(param);
+}
 
-			color = "transparent";
-			popover.animate(param, function() {
-				popover.css("color", color);
-			});
+function growDown(el) {
+	var param;
+
+	if (el.is(".info-overlay-top")) {
+		param = {opacity:0, height: "toggle"};
+	} else if (el.is(".info-overlay-bottom")) {
+		param = {opacity:0, height: "toggle"};
+	} else if (el.is(".info-overlay-left")) {
+		param = {opacity:0, width: "toggle"};
+	} else if (el.is(".info-overlay-right")) {
+		param = {opacity:0, width: "toggle"};
+	}
+
+	el.animate(param);
+}
+
+function toggle(el) {
+	var hardcodedWidth = 20;
+	var symbol;
+	var color;
+
+	if (el.height() == hardcodedWidth || el.width() == hardcodedWidth) {
+		// open el
+		if (el.is(".info-overlay-top")) {
+			var height = el.css("height", "auto").height();
+			// hack
+			el.height(hardcodedWidth);
+			symbol = "glyphicon glyphicon-chevron-up";
+			param = {height: height};
+		} else if (el.is(".info-overlay-bottom")) {
+			var height = el.css("height", "auto").height();
+			el.height(hardcodedWidth);
+			symbol = "glyphicon glyphicon-chevron-down";
+			param = {height: height};
+		} else if (el.is(".info-overlay-left")) {
+			var width = el.css("width", "auto").width();
+			el.width(hardcodedWidth + "px");
+			symbol = "glyphicon glyphicon-chevron-left";
+			param = {width: width};
+		} else if (el.is(".info-overlay-right")) {
+			var width = el.css("width", "auto").width();
+			el.width(hardcodedWidth + "px");
+			symbol = "glyphicon glyphicon-chevron-right";
+			param = {width: width};
 		}
 
-		toggle.find("span").removeClass();
-		toggle.find("span").addClass(symbol);
+		color = "black";
+		el.css("color", color);
+		el.animate(param)
 
+	} else {
+		// close el
+		if (el.is(".info-overlay-top")) {
+			symbol = "glyphicon glyphicon-chevron-down";
+			param = {height: hardcodedWidth + "px"};
+		} else if (el.is(".info-overlay-bottom")) {
+			symbol = "glyphicon glyphicon-chevron-up";
+			param = {height: hardcodedWidth + "px"};
+		} else if (el.is(".info-overlay-left")) {
+			symbol = "glyphicon glyphicon-chevron-right";
+			param = {width: hardcodedWidth + "px"};
+		} else if (el.is(".info-overlay-right")) {
+			symbol = "glyphicon glyphicon-chevron-left";
+			param = {width: hardcodedWidth + "px"};
+		}
+
+		color = "transparent";
+		el.animate(param, function() {
+			el.css("color", color);
+		});
+	}
+
+	el.find("span").removeClass();
+	el.find("span").addClass(symbol);
 }
